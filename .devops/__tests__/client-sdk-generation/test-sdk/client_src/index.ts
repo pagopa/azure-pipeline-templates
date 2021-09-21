5e6a5e16fd40d1780122323cd093318000132a4b
gquadrati <greta.quadrati@pagopa.it>
2021-09-21 16:09:51 +0200

    upgrade client tests libraries

--- .devops/__tests__/client-sdk-generation/test-sdk/client_src/index.ts
+++ .devops/__tests__/client-sdk-generation/test-sdk/client_src/index.ts
@@ -3,8 +3,10 @@ import {
   createClient,
 } from "./node_modules/@pagopa/api_test-sdk/client";
 
-const fetch = require('node-fetch');
+import { pipe } from "./node_modules/fp-ts/function";
+import * as E from "./node_modules/fp-ts/Either";
 
+const fetch = require("node-fetch");
 
 const baseUrl = "http://localhost:8088";
 
@@ -14,10 +16,10 @@ var client = createClient({
   fetchApi: fetch,
 }) as Client;
 
-client.getServerInfo({}).then(
-    v => v
-        .mapLeft(e => console.log(`Error ${e}`))
-        .map( val => console.log(`Get value: ${val.value.version}`))
-        
+client.getServerInfo({}).then((v) =>
+  pipe(
+    v,
+    E.mapLeft((e) => console.log(`Error ${e}`)),
+    E.map((val) => console.log(`Get value: ${val.value.version}`))
+  )
 );
-
