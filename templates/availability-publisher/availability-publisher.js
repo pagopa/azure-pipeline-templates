ddb5704624d3d3b7e3286ac64e1ed8805d431e14
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-04-28 11:11:36 +0200

    test no checkout

--- templates/availability-publisher/availability-publisher.js
+++ templates/availability-publisher/availability-publisher.js
@@ -4,7 +4,6 @@ appInsights.setup(process.env.APP_INSIGHT_CONNECTION_STRING).start();
 
 const client = new appInsights.TelemetryClient(process.env.APP_INSIGHT_CONNECTION_STRING);
 
-
 let event = {
   id: process.env.PIPELINE_NAME,
   success : (String(process.env.SUCCESS).toLowerCase() === 'true'),
